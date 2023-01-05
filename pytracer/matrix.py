5d8a94c344d79e416da794b8181fc1c93382b148
Chris Lawlor <238952+chrislawlor@users.noreply.github.com>
2023-01-05 10:46:35 -0500

    Implement matrices

---
+++ pytracer/matrix.py
@@ -0,0 +1,143 @@
+from __future__ import annotations
+
+from functools import cached_property
+
+from .primitives import FourTuple
+
+
+class Matrix:
+    def __init__(self, cells: list[list[float | int]]):
+        self.cells = cells
+        if cells:
+            width = len(cells[0])
+            if not all([len(row) == width for row in cells]):
+                raise ValueError(f" Cells have uneven rows: {cells}")
+            self.width = len(cells[0])
+            self.height = len(cells)
+        else:
+            self.width = 0
+            self.height = 0
+
+    @classmethod
+    def with_shape(cls, width: int, height: int, fill: int | float = 0) -> Matrix:
+        cells = [[fill] * width for _ in range(height)]
+        return cls(cells)
+
+    @classmethod
+    def identity(cls, size) -> Matrix:
+        """Build identity matrix of shape size x size"""
+        result = Matrix.with_shape(size, size)
+        for i in range(size):
+            result[i, i] = 1
+        return result
+
+    def transpose(self) -> Matrix:
+        """
+        Transpose, return a new Matrix.
+        """
+        result = Matrix.with_shape(self.width, self.height)
+        for row in range(self.height):
+            for col in range(self.width):
+                result[row, col] = self[col, row]
+        return result
+
+    @cached_property
+    def determinant(self) -> float:
+        """Calculate the determinant"""
+        if self.width == 2 and self.height == 2:
+            return self[0, 0] * self[1, 1] - self[0, 1] * self[1, 0]
+
+        return sum([self[0, col] * self.cofactor(0, col) for col in range(self.width)])
+
+    def submatrix(self, row: int, col: int) -> Matrix:
+        """
+        Return a new submatrix of this matrix, with the given
+        row and col removed
+        """
+        cells = []
+        for row_i in range(self.height):
+            if row_i != row:
+                new_row = []
+                for col_i in range(self.width):
+                    if col_i != col:
+                        new_row.append(self[row_i, col_i])
+                cells.append(new_row)
+        return Matrix(cells)
+
+    def minor(self, row: int, col: int) -> float:
+        """
+        Calculate the determinant of the submatrix at row, col.
+        """
+        submatrix = self.submatrix(row, col)
+        return submatrix.determinant
+
+    def cofactor(self, row: int, col: int) -> float:
+        # if row + col is an odd number, negate the minor
+        result = self.minor(row, col)
+        if (row + col) % 2 == 0:
+            return result
+        return -result
+
+    def is_invertible(self) -> bool:
+        return self.determinant != 0
+
+    def inverse(self) -> Matrix:
+        """Return the inverse of this matrix"""
+        if not self.is_invertible():
+            raise ValueError(f"Matrix is not invertible: {self}")
+        result = Matrix.with_shape(self.width, self.height)
+
+        for row in range(self.height):
+            for col in range(self.width):
+                c = self.cofactor(row, col)
+
+                # Note: "col, row" here, instead of "row, col",
+                # accomplishes the transpose operation
+                result[col, row] = c / self.determinant
+        return result
+
+    def __getitem__(self, key: tuple[int, int]) -> float | int:
+        col, row = key
+        return self.cells[col][row]
+
+    def __setitem__(self, key: tuple[int, int], value):
+        col, row = key
+        self.cells[col][row] = value
+
+    def __eq__(self, other) -> bool:
+        return (
+            isinstance(other, Matrix)
+            and self.width == other.width
+            and self.height == other.height
+            and self.cells == other.cells
+        )
+
+    def __mul__(self, other: Matrix | FourTuple):
+        if isinstance(other, Matrix):
+            return self._mul_matrix(other)
+        if isinstance(other, FourTuple):
+            return self._mul_tuple(other)
+        raise TypeError(f"Cannot multiply Matrix with type {type(other)}")
+
+    def _mul_matrix(self, other: Matrix) -> Matrix:
+        if self.width != other.height:
+            raise ValueError(
+                "Cannot multiply matrices where width of first is not equal "
+                "to height of the second"
+            )
+
+        result = self.with_shape(other.width, other.height)
+        for row in range(other.height):
+            for col in range(other.width):
+                result[row, col] = sum(
+                    [self[row, i] * other[i, col] for i in range(self.width)]
+                )
+        return result
+
+    def _mul_tuple(self, other: FourTuple) -> FourTuple:
+        result = self * Matrix([[other.x], [other.y], [other.z], [other.w]])
+        return FourTuple(*[c[0] for c in result.cells])
+
+    def __repr__(self) -> str:
+        return f"Matrix({self.cells})"
+        return f"Matrix({self.cells})"
---
+++ tests/test_matrix.py
@@ -0,0 +1,212 @@
+import pytest
+
+from pytracer.matrix import Matrix
+from pytracer.primitives import FourTuple
+
+EPSILON = 0.0001
+
+
+def assert_approx_equal(m1: Matrix, m2: Matrix):
+    assert m1.width == m2.width
+    assert m2.height == m2.height
+    for row in range(m1.height):
+        for col in range(m1.width):
+            assert m1[row, col] == pytest.approx(m2[row, col], abs=EPSILON)
+
+
+def test_getitem():
+    m = Matrix(
+        [[1, 2, 3, 4], [5.5, 6.5, 7.5, 8.5], [9, 10, 11, 12], [13.5, 14.5, 15.5, 16.5]]
+    )
+    assert m[0, 0] == 1
+    assert m[0, 3] == 4
+    assert m[1, 0] == 5.5
+    assert m[1, 2] == 7.5
+    assert m[2, 2] == 11
+    assert m[3, 3] == 16.5
+    with pytest.raises(IndexError):
+        m[4, 4]
+
+
+def test_equality():
+    m1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    m2 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    assert m1 == m2
+
+
+def test_inequality():
+    m1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    m2 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 10]])
+    assert m1 != m2
+
+
+def test_assignment():
+    m = Matrix.with_shape(3, 3)
+    m[0, 0] = 1
+    m[2, 2] = 4
+    assert m == Matrix([[1, 0, 0], [0, 0, 0], [0, 0, 4]])
+
+
+def test_multiplying_4x4_matrices():
+    m1 = Matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])
+    m2 = Matrix([[-2, 1, 2, 3], [3, 2, 1, -1], [4, 3, 6, 5], [1, 2, 7, 8]])
+    assert m1 * m2 == Matrix(
+        [[20, 22, 50, 48], [44, 54, 114, 108], [40, 58, 110, 102], [16, 26, 46, 42]]
+    )
+
+
+def test_multiplying_4x4_matrix_by_a_tuple():
+    m = Matrix([[1, 2, 3, 4], [2, 4, 4, 2], [8, 6, 4, 1], [0, 0, 0, 1]])
+    t = FourTuple(1, 2, 3, 1)
+
+    assert m * t == FourTuple(18, 24, 33, 1)
+
+
+def test_build_identity_matrix():
+    m = Matrix.identity(4)
+    assert m == Matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+
+
+def test_multiply_by_identity_matrix():
+    identity = Matrix.identity(4)
+    m = Matrix([list(range(4)) for _ in range(4)])
+    assert m * identity == m
+
+
+def test_transpose_identity_matrix_is_identity_matrix():
+    identity = Matrix.identity(4)
+    assert identity.transpose() == identity
+
+
+def test_transpose_matrix():
+    m = Matrix([[0, 9, 3, 0], [9, 8, 0, 8], [1, 8, 5, 3], [0, 0, 5, 8]])
+    assert m.transpose() == Matrix(
+        [[0, 9, 1, 0], [9, 8, 8, 0], [3, 0, 5, 5], [0, 8, 3, 8]]
+    )
+
+
+def test_calculate_determinant_of_2x2_matrix():
+    m = Matrix([[1, 5], [-3, 2]])
+    assert m.determinant == 17
+
+
+def test_submatrix_of_3x3_matrix_is_a_2x2_matrix():
+    m = Matrix([[1, 5, 0], [-3, 2, 7], [0, 6, -3]])
+    result = m.submatrix(0, 2)
+    assert result == Matrix([[-3, 2], [0, 6]])
+
+
+def test_submatrix_of_4x4_matrix_is_a_3x3_matrix():
+    m = Matrix([[-6, 1, 1, 6], [-8, 5, 8, 6], [-1, 0, 8, 2], [-7, 1, -1, 1]])
+    result = m.submatrix(2, 1)
+    assert result == Matrix([[-6, 1, 6], [-8, 8, 6], [-7, -1, 1]])
+
+
+def test_calculate_minor_of_3x3_matrix():
+    m = Matrix([[3, 5, 0], [2, -1, -7], [6, -1, 5]])
+    submatrix = m.submatrix(1, 0)
+    assert submatrix.determinant == 25
+    assert m.minor(1, 0) == 25
+
+
+def test_calculate_cofactor_of_3x3_matrix():
+    m = Matrix([[3, 5, 0], [2, -1, -7], [6, -1, 5]])
+    assert m.minor(0, 0) == -12
+    assert m.cofactor(0, 0) == -12
+    assert m.minor(1, 0) == 25
+    assert m.cofactor(1, 0) == -25
+
+
+def test_calculate_determinant_of_3x3_matrix():
+    m = Matrix([[1, 2, 6], [-5, 8, -4], [2, 6, 4]])
+    assert m.cofactor(0, 0) == 56
+    assert m.cofactor(0, 1) == 12
+    assert m.cofactor(0, 2) == -46
+    assert m.determinant == -196
+
+
+def test_calculate_determinant_of_4x4_matrix():
+    m = Matrix([[-2, -8, 3, 5], [-3, 1, 7, 3], [1, 2, -9, 6], [-6, 7, 7, -9]])
+    assert m.cofactor(0, 0) == 690
+    assert m.cofactor(0, 1) == 447
+    assert m.cofactor(0, 2) == 210
+    assert m.cofactor(0, 3) == 51
+    assert m.determinant == -4071
+
+
+def test_an_invertible_matrix_for_invertibility():
+    m = Matrix([[6, 4, 4, 4], [5, 5, 7, 6], [4, -9, 3, -7], [9, 1, 7, -6]])
+    assert m.determinant == -2120
+    assert m.is_invertible()
+
+
+def test_non_invertible_matrix_for_invertibility():
+    m = Matrix([[-4, 2, -2, 3], [9, 6, 2, 6], [0, -5, 1, -5], [0, 0, 0, 0]])
+    assert m.determinant == 0
+    assert not m.is_invertible()
+
+
+def test_calculate_the_inverse_of_a_matrix():
+    A = Matrix(
+        [
+            # fmt: off
+            [-5,  2,  6, -8],
+            [ 1, -5,  1,  8],
+            [ 7,  7, -6, -7],
+            [ 1, -3,  7,  4]
+            # fmt: on
+        ]
+    )
+
+    assert A.determinant == 532
+    assert A.cofactor(2, 3) == -160
+    assert A.cofactor(3, 2) == 105
+
+    B = A.inverse()
+    assert B[3, 2] == -160 / 532
+    assert B[2, 3] == 105 / 532
+
+    assert_approx_equal(
+        B,
+        Matrix(
+            [
+                # fmt: off
+                [ 0.21805,  0.45113,  0.24060, -0.04511],
+                [-0.80827, -1.45677, -0.44361,  0.52068],
+                [-0.07895, -0.22368, -0.05263,  0.19737],
+                [-0.52256, -0.81391, -0.30075,  0.30639]
+                # fmt: on
+            ]
+        ),
+    )
+
+
+def test_calculate_inverse_of_another_matrix():
+    m = Matrix(
+        [
+            # fmt: off
+        [ 8, -5,  9,  2],
+        [ 7,  5,  6,  1],
+        [-6,  0,  9,  6],
+        [-3,  0, -9, -4],
+            # fmt: on
+        ]
+    )
+    expected = Matrix(
+        [
+            # fmt: off
+        [-0.15385, -0.15385, -0.28205, -0.53846],
+        [-0.07692,  0.12308,  0.02564,  0.03077],
+        [ 0.35897,  0.35897,  0.43590,  0.92308],
+        [-0.69231, -0.69231, -0.76923, -1.92308],
+            # fmt: on
+        ]
+    )
+    assert_approx_equal(m.inverse(), expected)
+
+
+def test_multiply_a_product_by_its_inverse():
+    m1 = Matrix([[3, -9, 7, 3], [3, -8, 2, -9], [-4, 4, 4, 1], [-6, 5, -1, 1]])
+    m2 = Matrix([[8, 2, 2, 2], [3, -1, 7, 0], [7, 0, 5, 4], [6, -2, 0, 5]])
+    m3 = m1 * m2
+    assert_approx_equal(m3 * m2.inverse(), m1)
